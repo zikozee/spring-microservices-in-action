8ac692bb12ed68c5c590721e109e261a98acf1da
ezekiel.eromosei <ezekiel.eromosei@gmail.com>
2023-06-08 13:49:45 +0100

    add eureka client config closes #13

--- licensing-service/src/main/java/com/optimagrowth/license/LicensingServiceApplication.java
+++ licensing-service/src/main/java/com/optimagrowth/license/LicensingServiceApplication.java
@@ -2,7 +2,6 @@ package com.optimagrowth.license;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;

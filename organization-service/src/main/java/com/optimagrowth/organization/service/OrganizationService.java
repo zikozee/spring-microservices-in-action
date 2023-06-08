f7f85b679430c5c9363d88c7a89749463ebd31e5
ezekiel.eromosei <ezekiel.eromosei@gmail.com>
2023-06-08 11:40:45 +0100

    implement organisation service closes #12

---
+++ organization-service/src/main/java/com/optimagrowth/organization/service/OrganizationService.java
@@ -0,0 +1,38 @@
+package com.optimagrowth.organization.service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.optimagrowth.organization.model.Organization;
+import com.optimagrowth.organization.repository.OrganizationRepository;
+
+@Service
+@RequiredArgsConstructor
+public class OrganizationService {
+
+    private final OrganizationRepository repository;
+
+    public Organization findById(String organizationId) {
+    	Optional<Organization> opt = repository.findById(organizationId);
+        return (opt.isPresent()) ? opt.get() : null;
+    }
+
+    public Organization create(Organization organization){
+    	organization.setId( UUID.randomUUID().toString());
+        organization = repository.save(organization);
+        return organization;
+
+    }
+
+    public void update(Organization organization){
+    	repository.save(organization);
+    }
+
+    public void delete(Organization organization){
+    	repository.deleteById(organization.getId());
+    }
+}

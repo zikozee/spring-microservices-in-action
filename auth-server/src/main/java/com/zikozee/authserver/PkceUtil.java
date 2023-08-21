c20c9b698de0b2674f00c2ee5dc8763c184427ca
ezekiel.eromosei <ezekiel.eromosei@gmail.com>
2023-08-21 21:48:43 +0100

    added pcke code generator for auth server closes #30

---
+++ auth-server/src/main/java/com/zikozee/authserver/PkceUtil.java
@@ -0,0 +1,42 @@
+package com.zikozee.authserver;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+@Slf4j
+public class PkceUtil {
+    static String generateCodeVerifier() throws UnsupportedEncodingException {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] codeVerifier = new byte[32];
+        secureRandom.nextBytes(codeVerifier);
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(codeVerifier);
+    }
+
+    static String generateCodeChallenge(String codeVerifier) throws NoSuchAlgorithmException {
+        byte[] bytes = codeVerifier.getBytes(StandardCharsets.US_ASCII);
+        MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+        messageDigest.update(bytes, 0, bytes.length);
+        byte[] digest = messageDigest.digest();
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(digest);
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            String codeVerifier = generateCodeVerifier();
+            System.out.println("Code verifier = " + codeVerifier);
+
+            String codeChallenge = generateCodeChallenge(codeVerifier);
+            System.out.println("Code challenge = " + codeChallenge);
+
+        } catch (UnsupportedEncodingException | NoSuchAlgorithmException ex) {
+            log.error(ex.getMessage(), ex);
+        }
+    }
+}
